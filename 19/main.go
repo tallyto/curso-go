280d51603aa0dd57c336aaafb17e78268ba8da2c
tallyto <rodrigues.tallyto@hotmail.com>
2024-02-26 21:50:43 -0300

    Type assertion

---
+++ 19/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var minhaVar interface{} = "Tallyto Rodrigues"
+
+	println(minhaVar.(string))
+	res, ok := minhaVar.(int)
+	fmt.Printf("O valor de res é %v e o resultado é %v\n", res, ok)
+	res2 := minhaVar.(int)
+	fmt.Printf("O valor de res2 é %v", res2)
+}

e197167edf37b2cb59b5d88e63ba334977b243aa
Tallyto <rodrigues.tallyto@hotmail.com>
2024-03-13 15:52:10 -0300

    Compondo templates

---
+++ pacotes-importantes/11-compondo-templates/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
+func main() {
+	templates := []string{
+		"header.html",
+		"content.html",
+		"footer.html",
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmp := template.New("content.html")
+		tmp, _ = tmp.ParseFiles(templates...)
+		err := tmp.Execute(w, Cursos{
+			{"Go", 40},
+			{"Java", 30},
+			{"Python", 20},
+			{"C#", 10},
+			{"PHP", 5},
+			{"C++", 3},
+			{"Ruby", 2},
+		})
+		if err != nil {
+			panic(err)
+		}
+
+	})
+
+	http.ListenAndServe(":3001", nil)
+
+}

287dae207b4e977534393a34a31f4fddd9d3eb40
Tallyto <rodrigues.tallyto@hotmail.com>
2024-03-17 12:55:16 -0300

    Customizando Request

---
+++ pacotes-importantes/15-customizando-resquest/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	c := http.Client{}
+
+	req, err := http.NewRequest("GET", "http://google.com", nil)
+
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := c.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
+
+}

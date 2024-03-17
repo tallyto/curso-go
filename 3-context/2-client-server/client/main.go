8e136c2091dfc4fa2cf322db853f85e5fd33450d
Tallyto <rodrigues.tallyto@hotmail.com>
2024-03-17 20:47:29 -0300

    Context no lado do cliente

---
+++ 3-context/2-client-server/client/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:3002", nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	io.Copy(os.Stdout, res.Body)
+
+}

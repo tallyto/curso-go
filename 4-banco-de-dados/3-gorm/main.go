352652b22961d11bd8ea9177713436c7689dd064
Tallyto <rodrigues.tallyto@hotmail.com>
2024-03-18 10:45:24 -0300

    Realizando primeiras consultas

--- 4-banco-de-dados/3-gorm/main.go
+++ 4-banco-de-dados/3-gorm/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -19,32 +21,52 @@ func main() {
 		panic(err)
 	}
 
-	db.AutoMigrate(&Product{})
+	// db.AutoMigrate(&Product{})
 
 	// create
-	db.Create(&Product{
-		Name:  "Notebook",
-		Price: 1000,
-	})
+	// db.Create(&Product{
+	// 	Name:  "Notebook",
+	// 	Price: 1000,
+	// })
 
 	// create batch
-	products := []Product{
-		{Name: "Mouse", Price: 100},
-		{Name: "Keyboard", Price: 200},
-		{Name: "Monitor", Price: 300},
-		{Name: "Webcam", Price: 400},
-		{Name: "Microphone", Price: 500},
-		{Name: "Headset", Price: 600},
-		{Name: "Speaker", Price: 700},
-		{Name: "Pen Drive", Price: 800},
-		{Name: "Memory Card", Price: 900},
-		{Name: "USB Cable", Price: 1000},
-		{Name: "HDMI Cable", Price: 1100},
-		{Name: "DVD Drive", Price: 1200},
-		{Name: "SSD Drive", Price: 1300},
-		{Name: "Flash Drive", Price: 1400},
-		{Name: "Printer", Price: 1500},
-		{Name: "Scanner", Price: 1600},
+	// products := []Product{
+	// 	{Name: "Mouse", Price: 100},
+	// 	{Name: "Keyboard", Price: 200},
+	// 	{Name: "Monitor", Price: 300},
+	// 	{Name: "Webcam", Price: 400},
+	// 	{Name: "Microphone", Price: 500},
+	// 	{Name: "Headset", Price: 600},
+	// 	{Name: "Speaker", Price: 700},
+	// 	{Name: "Pen Drive", Price: 800},
+	// 	{Name: "Memory Card", Price: 900},
+	// 	{Name: "USB Cable", Price: 1000},
+	// 	{Name: "HDMI Cable", Price: 1100},
+	// 	{Name: "DVD Drive", Price: 1200},
+	// 	{Name: "SSD Drive", Price: 1300},
+	// 	{Name: "Flash Drive", Price: 1400},
+	// 	{Name: "Printer", Price: 1500},
+	// 	{Name: "Scanner", Price: 1600},
+	// }
+	// db.Create(&products)
+
+	// select one
+
+	// var product Product
+	// db.First(&product, 2)
+	// fmt.Println(product)
+
+	// db.First(&product, "name = ?", "Monitor")
+	// fmt.Println(product)
+
+	// select all
+
+	var products []Product
+
+	db.Find(&products)
+
+	for _, product := range products {
+		fmt.Println(product)
 	}
-	db.Create(&products)
+
 }

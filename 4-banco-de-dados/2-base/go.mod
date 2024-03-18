c986b545f852b153246065e505de2b2cada16038
Tallyto <rodrigues.tallyto@hotmail.com>
2024-03-18 09:25:04 -0300

    Inserindo dados no banco

--- 4-banco-de-dados/2-base/go.mod
+++ 4-banco-de-dados/2-base/go.mod
@@ -2,4 +2,9 @@ module github.com/tallyto/curso-go/4-banco-de-dados/2-base
 
 go 1.22.1
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/go-sql-driver/mysql v1.8.0
+	github.com/google/uuid v1.6.0
+)
+
+require filippo.io/edwards25519 v1.1.0 // indirect
--- 4-banco-de-dados/2-base/main.go
+++ 4-banco-de-dados/2-base/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
 
 type Product struct {
 	ID    string
@@ -8,7 +13,7 @@ type Product struct {
 	Price float64
 }
 
-func NewProduct(id, name string, price float64) *Product {
+func NewProduct(name string, price float64) *Product {
 	return &Product{
 		ID:    uuid.New().String(),
 		Name:  name,
@@ -17,5 +22,30 @@ func NewProduct(id, name string, price float64) *Product {
 }
 
 func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
 
+	defer db.Close()
+
+	product := NewProduct("TV", 1000.0)
+
+	insertProduct(db, product)
+
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("INSERT INTO products (id, name, price) VALUES (?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }

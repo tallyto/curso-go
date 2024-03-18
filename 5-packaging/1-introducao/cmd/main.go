52b88ed88b72bd128aea9f6a3833b2fe20b1ac4d
Tallyto <rodrigues.tallyto@hotmail.com>
2024-03-18 17:33:45 -0300

    Acessando pacotes criados

--- 5-packaging/1-introducao/cmd/main.go
+++ 5-packaging/1-introducao/cmd/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/tallyto/curso-go/5-packaging/1-introducao/math"
+)
 
 func main() {
+	m := math.Math{A: 1, B: 2}
+
+	fmt.Println(m.Add())
 	fmt.Println("Hello World!")
 }

7ad6b16e5c99ca594c6ac8f8b99c70888ccee774
tallyto <rodrigues.tallyto@hotmail.com>
2024-04-01 12:40:11 -0300

    Trabalhando com Mocks

--- 6-testing/2/tax_test.go
+++ 6-testing/2/tax_test.go
@@ -1,10 +1,27 @@
 package tax
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestCalculateTaxAndSave(t *testing.T) {
+	repository := &TaxRepositoryMock{}
+	repository.On("SaveTax", 10.0).Return(nil).Once()
+	repository.On("SaveTax", 0.0).Return(errors.New("error saving tax"))
+
+	err := CalculateTaxAndSave(1000.0, repository)
+	assert.Nil(t, err)
+
+	err = CalculateTaxAndSave(0.0, repository)
+	assert.Error(t, err, "error saving tax")
+
+	repository.AssertExpectations(t)
+	repository.AssertNumberOfCalls(t, "SaveTax", 2)
+
+}
 
 func TestCalculateTax(t *testing.T) {
 	tax, err := CalculateTax(1000)

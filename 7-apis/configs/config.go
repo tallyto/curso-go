caad6365c2f24148b6da2117a652c94709335d63
tallyto <rodrigues.tallyto@hotmail.com>
2024-04-01 15:29:54 -0300

    Criando arquivo de configuração

---
+++ 7-apis/configs/config.go
@@ -0,0 +1,19 @@
+package config
+
+var cfg *conf
+
+type conf struct {
+	DBDriver      string
+	DBHost        string
+	DBPort        string
+	DBUser        string
+	DBPassword    string
+	DBName        string
+	WebServerPort string
+	JWTSecret     string
+	JwtExpiresIn  int
+}
+
+func LoadConfig(path string) (*conf, error) {
+
+}
---
+++ 7-apis/go.mod
@@ -0,0 +1,3 @@
+module github.com/tallyto/curso-go/7-apis
+
+go 1.22.1

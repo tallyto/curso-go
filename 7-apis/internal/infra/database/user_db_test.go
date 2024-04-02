30c065a9868f97df60642165e89f59af757cef8e
tallyto <rodrigues.tallyto@hotmail.com>
2024-04-02 14:21:29 -0300

    Finalizando tesde de UserDB

--- 7-apis/internal/infra/database/user_db_test.go
+++ 7-apis/internal/infra/database/user_db_test.go
@@ -32,3 +32,24 @@ func TestCreateUser(t *testing.T) {
 	assert.NotEmpty(t, user.Password)
 
 }
+
+func TestFindByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("John Doe", "john.doe@example.com", "password@123")
+	userDB := NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.FindByEmail(user.Email)
+
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotEmpty(t, user.Password)
+}

6f352aaa1b287f9c315eb09a8f97c73e49323946
tallyto <rodrigues.tallyto@hotmail.com>
2024-04-02 14:11:44 -0300

    Testando criação do usuário

--- 7-apis/internal/infra/database/user_db.go
+++ 7-apis/internal/infra/database/user_db.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"github.com/tallyto/curso-go/7-apis/internal/entity"
 	"gorm.io/gorm"
 )
 
@@ -10,4 +11,17 @@ type User struct {
 
 func NewUser(db *gorm.DB) *User {
 	return &User{DB: db}
+}
+
+func (u *User) Create(user *entity.User) error {
+	return u.DB.Create(user).Error
+}
+
+func (u *User) FindByEmail(email string) (*entity.User, error) {
+	var user entity.User
+	err := u.DB.Where("email = ?", email).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
---
+++ 7-apis/internal/infra/database/user_db_test.go
@@ -0,0 +1,34 @@
+package database
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tallyto/curso-go/7-apis/internal/entity"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestCreateUser(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("John Doe", "john.doe@example.com", "password@123")
+	userDB := NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	var userFound entity.User
+
+	err = db.First(&userFound, "id = ?", user.ID).Error
+
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotEmpty(t, user.Password)
+
+}

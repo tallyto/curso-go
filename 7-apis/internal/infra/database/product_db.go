ba53389f980d1ea2d83e539888d733e2a468e2fc
tallyto <rodrigues.tallyto@hotmail.com>
2024-04-07 15:53:26 -0300

    Criando pricipais métodos de ProductDB

---
+++ 7-apis/internal/infra/database/product_db.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"github.com/tallyto/curso-go/7-apis/internal/entity"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	DB *gorm.DB
+}
+
+func NewProduct(db *gorm.DB) *Product {
+	return &Product{DB: db}
+}
+
+func (p *Product) Create(product *entity.Product) error {
+	return p.DB.Create(product).Error
+}
+
+func (p *Product) FindById(id string) (*entity.Product, error) {
+	var product entity.Product
+	err := p.DB.First(&product, "id = ?", id).Error
+	return &product, err
+}
+
+func (p *Product) Update(product *entity.Product) error {
+	_, err := p.FindById(product.ID.String())
+	if err != nil {
+		return err
+	}
+	return p.DB.Save(product).Error
+}
+
+func (p *Product) Delete(id string) error {
+	product, err := p.FindById(id)
+	if err != nil {
+		return err
+	}
+	return p.DB.Delete(product).Error
+}

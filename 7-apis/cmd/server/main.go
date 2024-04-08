8111a075b981d78b5ba03465310e701189619e47
tallyto <rodrigues.tallyto@hotmail.com>
2024-04-08 10:33:33 -0300

    Ajustando package para os handlers

--- 7-apis/cmd/server/main.go
+++ 7-apis/cmd/server/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/tallyto/curso-go/7-apis/configs"
-	"github.com/tallyto/curso-go/7-apis/internal/dto"
 	"github.com/tallyto/curso-go/7-apis/internal/entity"
 	"github.com/tallyto/curso-go/7-apis/internal/infra/database"
+	"github.com/tallyto/curso-go/7-apis/internal/infra/webserver/handlers"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -27,42 +26,9 @@ func main() {
 	db.AutoMigrate(&entity.Product{}, &entity.User{})
 	productDB := database.NewProduct(db)
 
-	productHandler := NewProductHandler(productDB)
+	productHandler := handlers.NewProductHandler(productDB)
 	http.HandleFunc("/products", productHandler.CreateProduct)
 
 	http.ListenAndServe(":3001", nil)
 
-}
-
-type ProductHandler struct {
-	ProductDB database.ProductInterface
-}
-
-func NewProductHandler(db database.ProductInterface) *ProductHandler {
-	return &ProductHandler{
-		ProductDB: db,
-	}
-}
-
-func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
-	var product dto.CreateProductInput
-	err := json.NewDecoder(r.Body).Decode(&product)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	p, err := entity.NewProduct(product.Name, product.Price)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	err = h.ProductDB.Create(p)
-
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
 }
